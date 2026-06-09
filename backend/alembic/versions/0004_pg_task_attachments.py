13ef80d097eb94ac2b02768384ecca6221c757b4
theolek2 <theolek2@gmail.com>
2026-06-09 20:51:55 +0200

    fix: migration revision IDs

--- backend/alembic/versions/0004_pg_task_attachments.py
+++ backend/alembic/versions/0004_pg_task_attachments.py
@@ -1,7 +1,7 @@
 """task attachments
 
-Revision ID: 0004
-Revises: 0003
+Revision ID: 0004_pg_task_attachments
+Revises: 0003_pg_map_state
 Create Date: 2026-06-08
 
 """
@@ -9,8 +9,8 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "0004"
-down_revision: Union[str, None] = "0003"
+revision: str = "0004_pg_task_attachments"
+down_revision: Union[str, None] = "0003_pg_map_state"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
